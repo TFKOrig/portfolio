a2b337889052d2df30f975d3fa962241099c7764
Talha Farooq Khan <96606224+TFKOrig@users.noreply.github.com>
2024-02-08 11:04:32 +0500

    Add files via upload

---
+++ src/db/projectdb.js
@@ -0,0 +1,61 @@
+import {
+  css,
+  express,
+  html,
+  javascript,
+  mongodb,
+  node,
+  react,
+} from "./techstack";
+
+export const ProjectList = [
+  {
+    heading: "Audiophile",
+    img: "https://github.com/TFKOrig/Audiophile/blob/main/readmeImages/Audiophile.png?raw=true",
+    title: "Ecommerce Website",
+    description:
+      "It provides means of purchasing electronics like headphones,speakers",
+    techStack: [react, javascript, css, html, node, express, mongodb],
+    gitLink: "https://github.com/TFKOrig/Audiophile",
+  },
+
+  {
+    heading: "Social Proof",
+    img: "https://github.com/TFKOrig/SocialProof/blob/main/readmeImages/SocialProof.png?raw=true",
+    title: "Community Reviews",
+    description: "Community reviews about services.",
+    techStack: [css, html],
+    gitLink: "https://github.com/TFKOrig/SocialProof",
+    liveLink: "https://social-proof-by-tfk.netlify.app/",
+  },
+
+  {
+    heading: "TODOAPP ",
+    img: "https://github.com/TFKOrig/codealpha_tasks/blob/main/CodeAlpha_TODOAPP/readmeImages/ToDOAPP.png?raw=true",
+    title: "TODO-LIST",
+    description: "Add & Organize your daily tasks.",
+    techStack: [css, html, javascript],
+    gitLink:
+      "https://github.com/TFKOrig/codealpha_tasks/tree/main/CodeAlpha_TODOAPP",
+    liveLink: "https://codealphatodoapp.netlify.app/",
+  },
+
+  {
+    heading: "Classy Weather",
+    img: "https://github.com/TFKOrig/codealpha_tasks/blob/main/codealpha_weatherapp/readmeImages/Weather.png?raw=true",
+    title: "WeatherApp",
+    description: "Get to know your weekly weather in simple yet effective way.",
+    techStack: [css, html, javascript, react],
+    gitLink:
+      "https://github.com/TFKOrig/codealpha_tasks/tree/main/codealpha_weatherapp",
+  },
+
+  {
+    heading: "Fast React Pizza",
+    img: "https://github.com/TFKOrig/Pizzamenu/blob/main/readmeImages/pizzamenu.png?raw=true",
+    title: "Pizza Menu",
+    description: "Get your pizzas within 30mins of order.",
+    techStack: [css, html, javascript, react],
+    gitLink: "https://github.com/TFKOrig/Pizzamenu",
+  },
+];
---
+++ src/db/techstack.js
@@ -0,0 +1,72 @@
+import { SiExpress } from "react-icons/si";
+
+export const html = {
+  title: "HTML",
+  img: "https://cdn-icons-png.flaticon.com/512/1532/1532556.png",
+  wid: "70%",
+  swid: "40%",
+};
+
+export const css = {
+  title: "CSS",
+  img: "https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png",
+  wid: "65%",
+  swid: "40%",
+};
+
+export const javascript = {
+  title: "JavaScript",
+  img: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+  wid: "60%",
+  swid: "40%",
+};
+
+export const react = {
+  title: "React.js",
+  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  wid: "75%",
+  swid: "40%",
+};
+
+export const node = {
+  title: "Node.js",
+  img: "https://cdn-icons-png.flaticon.com/512/5968/5968322.png",
+  wid: "65%",
+  swid: "40%",
+};
+
+export const mongodb = {
+  title: "MongoDB",
+  img: "https://devkico.itexto.com.br/wp-content/uploads/2013/10/mongodb-leaf-300x300.png",
+  wid: "65%",
+  swid: "40%",
+};
+
+export const express = {
+  title: "Express",
+  img: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+  svg: SiExpress,
+  wid: "60%",
+  swid: "40%",
+};
+
+export const git = {
+  title: "Git",
+  img: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+  wid: "65%",
+  swid: "40%",
+};
+
+export const python = {
+  title: "Python",
+  img: "https://img.freepik.com/free-icon/snakes_318-368381.jpg",
+  wid: "70%",
+  swid: "40%",
+};
+
+export const mySQL = {
+  title: "MySQL",
+  img: "https://www.notion.so/image/https%3A%2F%2Fpngimg.com%2Fd%2Fmysql_PNG9.png?table=block&id=27e6b5e3-e888-4802-9c0b-2e5ca6bdf31a&spaceId=5247e3c7-a9ff-4596-9c82-e08b13f4ea9d&width=250&userId=af1a9bd4-c5ca-4c4b-88f4-f0e87ce2512a&cache=v2",
+  wid: "70%",
+  swid: "35%",
+};
